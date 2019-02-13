d1238a161e67121e0b707d011485f6229eda91d9
FarFarAways <guntapat.nak@mail.kmutt.ac.th>
2019-02-13 23:06:16 +0700

    ฟีดข่าว

--- front-end/src/views/News.vue
+++ front-end/src/views/News.vue
@@ -1,36 +1,59 @@
 <template>
     <v-card>
+      <br><br><br>
+      
         <v-container fluid grid-list-lg>
             <v-layout row wrap>
                 <v-flex xs12>
                     <v-card color="cyan darken-2" class="white--text">
                         <v-layout>
-                  <v-flex xs5>
-                    <v-img
-                      src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
-                      height="125px"
-                      contain
-                    ></v-img>
+                          
+                    <v-flex xs1>
+                 
+                  </v-flex>
+                  
+                  <v-flex xs2 md1>
+                    <br> <br>
+                    <v-icon size="50px">account_circle</v-icon>
+                   
                   </v-flex>
                   <v-flex xs7>
                     <v-card-title primary-title>
                       <div>
-                        <div class="headline">Supermodel</div>
-                        <div>Foster the People</div>
-                        <div>(2014)</div>
+                        <div class="headline">The Nun</div>
+                        <div>3 นาที</div>
+                        
                       </div>
                     </v-card-title>
+                    <div>ถนนประชาอุทิศ ซอยประชาอุทิศ 37  
+                            รถจยย. ชนกับ รถปิคอัพ ขวางช่องทางซ้าย 
+                              บาดเจ็บ 2 คน การจราจรเคลื่อนตัวช้า</div>
                   </v-flex>
+                  
                 </v-layout>
                 <v-divider light></v-divider>
+                
                 <v-card-actions class="pa-3">
-                  Rate this album
-                  <v-spacer></v-spacer>
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
-                  <v-icon>star_border</v-icon>
+                  <v-flex xs1>
+                  
+                  </v-flex>
+                   <v-flex xs2 md1    >
+                  <v-icon size="30px">account_circle</v-icon>
+                  </v-flex>
+                  
+                  <v-flex xs10>
+                   
+                        <v-card dark color="white black--text">
+                        <v-card-text>รถติดมาก
+                          <div class="text-xs-right" > 
+                            <v-btn round color="light-blue accent-4 white--text" v-on:click="getRoute" fab >
+                          <v-icon>send</v-icon></v-btn>
+                          </div>
+                          </v-card-text>
+                         
+                          </v-card>
+                        </v-flex>
+                  
                 </v-card-actions>
                     </v-card>
                 </v-flex>
