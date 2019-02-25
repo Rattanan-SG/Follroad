0fa79cefca2c502cfd09148729c4b523fecdd5ad
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-02-25 18:42:33 +0700

    [UPDATE] dockerize backend longdo service

---
+++ back-end/LongdoService/setup_database/seed/init.sql
@@ -0,0 +1,16 @@
+create table if not exists external_events
+(
+  eid            int          not null
+    primary key,
+  title          varchar(256) not null,
+  title_en       varchar(256) null,
+  description    text         not null,
+  description_en text         null,
+  type           int          not null,
+  longitude      double       not null,
+  latitude       double       not null,
+  start          datetime     not null,
+  stop           datetime     not null,
+  contributor    varchar(256) null,
+  icon           varchar(256) null
+);
