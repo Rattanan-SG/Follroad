f57517ed7584058dfb458da19166e4e31d33d4da
rattanan.nan@mail.kmutt.ac.th <rattanan.nan@mail.kmutt.ac.th>
2019-03-17 12:06:41 +0700

    [FIX] change back-end service name

---
+++ back-end/ExternalEventService/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const CronJob = require("cron").CronJob;
+const app = express();
+
+const db = require("./database/controller");
+const service = require("./controller/appService");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const job = new CronJob("*/3 * * * *", () => {
+  service.updateEventToDatabase();
+});
+
+job.start();
+
+app.get("/events", (req, res) => {
+  db.getEvents().then(events => {
+    res.send(events);
+  });
+});
+
+app.post("/update", (req, res) => {
+  service.updateEventToDatabase().then(out => {
+    res.send({ count: out });
+  });
+});
+
+app.post("/job/start", (req, res) => {
+  job.start();
+  console.log("Update data job has start");
+  res.sendStatus(200);
+});
+
+app.post("/job/stop", (req, res) => {
+  job.stop();
+  console.log("Update data job has stop");
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
