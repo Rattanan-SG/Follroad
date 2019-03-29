afc5dc41f25b4cb4fa82d1f74b3b9b98ee8deb4c
Chollachart <winnerzaza@gmail.com>
2019-03-29 10:43:00 +0700

    edit feed item and text description in feed

--- front-end/src/components/FeedItem.vue
+++ front-end/src/components/FeedItem.vue
@@ -1,11 +1,10 @@
 <template>
   <v-card>
-    <v-container fluid grid-list-lg>
+    <v-container align-center>
       <v-layout row wrap align-start justify-center fill-height>
-        <v-card color="cyan darken-2 white--text" class="mb-4 mt-4">
+        <v-card color="cyan darken-2 white--text" class="mb-4 mt-4" width="800px">
           <v-layout>
             <v-flex xs2 md1>
-              <br>
               <br>
               <v-icon size="50px" class="ml-4">account_circle</v-icon>
             </v-flex>
@@ -19,7 +18,7 @@
             </v-flex>
           </v-layout>
           <v-flex xs12>
-            <div>{{event.description}}</div>
+            <div id="description">{{event.description}}</div>
           </v-flex>
           <v-divider light></v-divider>
 
@@ -30,7 +29,8 @@
 
             <v-flex xs11>
               <v-card dark color="white black--text">
-                <v-card-text>รถติดมาก
+                <v-card-text>
+                  รถติดมาก
                   <div class="text-xs-right">
                     <v-btn round color="light-blue accent-4 white--text" fab>
                       <v-icon>send</v-icon>
@@ -49,6 +49,12 @@
 <style>
 .abc {
   margin-top: 2px;
+}
+#description {
+  margin-left: 30px;
+  margin-right: 30px;
+  color: white;
+  font-size: 17px;
 }
 </style>
 
