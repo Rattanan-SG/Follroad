d7ce8cfdf0a044214c9923384a3f7e76c8e67a80
rattanan.nan@mail.kmutt.ac.th <rattanan.nan@mail.kmutt.ac.th>
2019-03-30 16:06:52 +0700

    [UPDATE] get events limit and by type

--- back-end/ExternalEventService/app.js
+++ back-end/ExternalEventService/app.js
@@ -21,8 +21,24 @@ const job = new CronJob("*/3 * * * *", () => {
 
 job.start();
 
-app.get("/events", (req, res) => {
-  db.getEvents()
+app.get("/events", async (req, res) => {
+  let limit = parseInt(req.query.limit);
+  let startAt = req.query.startAt;
+  let events;
+  try {
+    if (limit && startAt) {
+      events = await db.getEventsHaveLimit(startAt, limit);
+    } else {
+      events = await db.getEvents();
+    }
+    res.send(events);
+  } catch (err) {
+    res.status(500).send({ error: `${err}` });
+  }
+});
+
+app.get("/events/type/:type", (req, res) => {
+  db.getEventByType(req.params.type)
     .then(events => {
       res.send(events);
     })
