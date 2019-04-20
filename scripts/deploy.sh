8198858d214d5ebd3659237a064edaca61cfdc9c
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-04-20 22:55:05 +0700

    [TEST] cicd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,4 +8,6 @@ apt-get install jq -y
 curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | sudo tee -a /usr/bin/ecs-deploy
 sudo chmod +x /usr/bin/ecs-deploy
 
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+
 ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $DOCKER_USERNAME/follroad-external-event:latest
