ed1409a0c0f30833e4f3781337600aa65b5bf1c2
FarFarAways <guntapat.nak@mail.kmutt.ac.th>
2019-04-26 16:33:02 +0700

    frame

--- front-end/src/components/SearchDirection.vue
+++ front-end/src/components/SearchDirection.vue
@@ -34,6 +34,30 @@
           <v-btn color="blue-grey" class="white--text" @click="startDirections">ค้นหาเส้นทาง</v-btn>
         </v-layout>
       </v-flex>
+      <v-flex xs11 pl-5 >
+      <v-card>
+        <v-card>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <div> {{ card_text }} </div>
+          </div>
+        </v-card-title>
+        </v-card>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <div> {{ card_text }} </div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
     </v-layout>
   </div>
 </template>
