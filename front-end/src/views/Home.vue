32561c66fe575c84e7542c89c3b05f0571142a61
Chollachart <winnerzaza@gmail.com>
2019-05-02 12:04:07 +0700

    [ADD]Show specific event at home menu and responsive

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    <SearchEvent/>
-    <FilterEventCategory/>
+    <v-layout row wrap>
+      <v-flex>
+        <SearchEvent/>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs7 lg8 md7 xl7 ml-1>
+        <ShowSpecificEvent/>
+      </v-flex>
+      <v-flex xs2 lg2 md2 xl2>
+        <FilterEventCategory/>
+      </v-flex>
+    </v-layout>
     <HomeFeedList></HomeFeedList>
   </div>
 </template> 
@@ -10,12 +21,14 @@
 import HomeFeedList from "../components/HomeFeedList";
 import FilterEventCategory from "../components/FilterEventCategory";
 import SearchEvent from "../components/SearchEvent";
+import ShowSpecificEvent from "../components/ShowSpecificEvent";
 export default {
   name: "Home",
   components: {
     HomeFeedList,
     FilterEventCategory,
-    SearchEvent
+    SearchEvent,
+    ShowSpecificEvent
   }
 };
 </script>
