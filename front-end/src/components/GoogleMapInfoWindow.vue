9ac25002d70a2966f69b246ec8ab6e8f5495911e
Chollachart <winnerzaza@gmail.com>
2019-06-11 17:19:16 +0700

    [EDIT]Infowindows to beautiful

--- front-end/src/components/GoogleMapInfoWindow.vue
+++ front-end/src/components/GoogleMapInfoWindow.vue
@@ -6,7 +6,79 @@
     :opened="infoWindow.infoWindowOpen"
     @closeclick="closeInfoWindow"
   >
-    <h2>{{infoWindow.marker.title}}</h2>
+    <v-toolbar
+      absolute
+      color="teal lighten-3"
+      dark
+      scroll-off-screen
+      scroll-target="#scrolling-techniques"
+      height="40%"
+    >
+      <v-flex xs1>
+        <v-btn icon dark @click="closeInfoWindow">
+          <v-icon>close</v-icon>
+        </v-btn>
+      </v-flex>
+      <v-flex xs9></v-flex>
+      <v-flex xs2>
+        <span>รายงาน</span>
+        <v-btn icon>
+          <v-icon>report</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-toolbar>
+    <br>
+    <br>
+    <br>
+    <v-card>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card color="cyan gradient" class="white--text">
+            <v-layout>
+              <v-flex xs5>
+                <!--ไว้ใส่รูปที่  user แจ้งเข้ามา-->
+              </v-flex>
+              <v-flex xs7>
+                <v-card-title primary-title>
+                  <div>
+                    <h3>{{infoWindow.marker.title}}</h3>
+                    <br>
+                    <div>{{infoWindow.marker.description}}</div>
+                    <br>
+                    <v-divider></v-divider>
+                    <br>
+                    <template v-if="infoWindow.marker.eventCaption">
+                      <p>เกิดเหตุการณ์ขึ้น ณ : {{infoWindow.marker.eventCaption.startTime}}</p>
+                      <p>จะสิ้นสุดใน : {{infoWindow.marker.eventCaption.stopTime}}</p>
+                      <p>ลงข้อมูลโดย : {{infoWindow.marker.eventCaption.contributor}}</p>
+                    </template>
+                    <template v-if="infoWindow.marker.searchPlaceCaption">
+                      <img
+                        :src="infoWindow.marker.searchPlaceCaption.photo"
+                        :alt="infoWindow.marker.title"
+                        width="350"
+                        height="200"
+                      >
+                    </template>
+                  </div>
+                </v-card-title>
+              </v-flex>
+            </v-layout>
+            <v-divider light></v-divider>
+            <v-card-actions class="pa-3">
+              ให้คะแนนเหตุการณ์
+              <v-spacer></v-spacer>
+              <v-icon>star_border</v-icon>
+              <v-icon>star_border</v-icon>
+              <v-icon>star_border</v-icon>
+              <v-icon>star_border</v-icon>
+              <v-icon>star_border</v-icon>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-card>
+    <!-- <h2>{{infoWindow.marker.title}}</h2>
     <br>
     <p>{{infoWindow.marker.description}}</p>
     <v-divider/>
@@ -23,7 +95,7 @@
         width="350"
         height="200"
       >
-    </template>
+    </template>-->
   </gmap-info-window>
 </template>
 
@@ -31,6 +103,11 @@
 import { mapGetters, mapActions } from "vuex";
 export default {
   name: "GoogleMapInfoWindow",
+  data() {
+    return {
+      dialog: false
+    };
+  },
   computed: {
     ...mapGetters(["infoWindow"])
   },
