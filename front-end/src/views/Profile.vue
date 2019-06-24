3dd701b12ef37902f334626f13f9a098d46c8fac
FarFarAways <guntapat.nak@mail.kmutt.ac.th>
2019-06-24 10:03:54 +0700

    [Update] Profile

--- front-end/src/views/Profile.vue
+++ front-end/src/views/Profile.vue
@@ -1,6 +1,70 @@
-<template></template>
+<template>
+  <!-- <v-flex xs12>
+    <div>
+      <v-icon large>keyboard_arrow_left</v-icon>
+    </div>
+  </v-flex>-->
+  <v-container>
+    <v-layout grid-list-md text-xs-center>
+      <v-flex xs12>
+        <v-btn size="60px" fab>
+          <v-icon>person</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout grid-list-md text-xs-center>
+      <v-flex xs12>
+        <h1 class="Name">Vertical Duke</h1>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 grid-list-md text-xs-center>
+        <h4 class="e-mail">vertical.duke@gmail.com</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 lg12 mt-5>
+        <v-list dense>
+          <v-list-tile @click>
+            <v-list-tile-content>
+              <h2>ชื่อ เบอร์โทรศัพท์ อีเมล</h2>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click>
+            <v-list-tile-content>
+              <h2>ชื่อ เบอร์โทรศัพท์ อีเมล</h2>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click>
+            <v-list-tile-content>
+              <h2>ชื่อ เบอร์โทรศัพท์ อีเมล</h2>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 lg12 mt-5>
+        <v-list dense>
+          <v-list-tile @click>
+            <v-list-tile-content>
+              <h2>ชื่อ เบอร์โทรศัพท์ อีเมล</h2>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click>
+            <v-list-tile-content>
+              <h2>ชื่อ เบอร์โทรศัพท์ อีเมล</h2>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click>
+            <v-list-tile-content>
+              <h2>ชื่อ เบอร์โทรศัพท์ อีเมล</h2>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
 <script>
-// export default {
-//   name: "Profile",
-// };
 </script>
