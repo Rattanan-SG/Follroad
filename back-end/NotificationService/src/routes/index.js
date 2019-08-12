2e1ff9c36d42b5e319804aa1999f329581ae1ddb
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-08-12 20:06:45 +0700

    [FIX] word

--- back-end/NotificationService/src/routes/index.js
+++ back-end/NotificationService/src/routes/index.js
@@ -3,21 +3,21 @@ const api = require("./api");
 
 const router = express.Router();
 
-const { npm_package_name, npm_package_version } = process.env;
+const { npm_package_name: service, npm_package_version: version } = process.env;
 
 router.get("/", (req, res) => {
-  res.json(`Hello this is ${npm_package_name}`);
+  res.json(`Hello this is ${service}`);
 });
 
 router.get("/notification", (req, res) => {
-  res.json(`Hello this is ${npm_package_name}`);
+  res.json(`Hello this is ${service}`);
 });
 
 router.get("/notification/health-check", (req, res) => {
   res.json({
     status: "OK",
-    service: npm_package_name,
-    version: npm_package_version
+    service,
+    version
   });
 });
 
@@ -26,8 +26,8 @@ router.use("/notification/api", api);
 router.all("*", (req, res) => {
   res.status(404).json({
     message: "Not Found",
-    service: npm_package_name,
-    version: npm_package_version
+    service,
+    version
   });
 });
 
