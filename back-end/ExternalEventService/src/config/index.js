72e03a204e15e4092c56c232488b36613a825f7c
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-08-15 13:53:28 +0700

    [FIX] config db

--- back-end/ExternalEventService/src/config/index.js
+++ back-end/ExternalEventService/src/config/index.js
@@ -10,7 +10,8 @@ const config = {
     db_host: "database",
     db_user: "admin",
     db_password: "dev",
-    db_database: "dev"
+    db_database: "dev",
+    db_connection_limit: 5
   },
   testing: {
     config_id: "testing"
@@ -21,7 +22,8 @@ const config = {
     db_host: env.DB_HOST,
     db_user: env.DB_USER,
     db_password: env.DB_PASSWORD,
-    db_database: env.DB_DATABASE
+    db_database: env.DB_DATABASE,
+    db_connection_limit: env.DB_CONNECTION_LIMIT || 5
   },
   production: {
     config_id: "production",
@@ -29,7 +31,8 @@ const config = {
     db_host: env.DB_HOST,
     db_user: env.DB_USER,
     db_password: env.DB_PASSWORD,
-    db_database: env.DB_DATABASE
+    db_database: env.DB_DATABASE,
+    db_connection_limit: env.DB_CONNECTION_LIMIT || 5
   }
 };
 
