d6c36212701f48233889bfacce92cdecf2409698
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-08-18 20:32:03 +0700

    [INIT] internal-event-service

---
+++ back-end/InternalEventService/src/controllers/index.js
@@ -0,0 +1,27 @@
+// const asyncWrapper = require("../middleware/async-wrapper");
+// const service = require("../services");
+
+// exports.createRecord = asyncWrapper(async (req, res) => {
+//   const result = await service.createRecord(req.body);
+//   res.send(result);
+// });
+
+// exports.getRecord = asyncWrapper(async (req, res) => {
+//   const result = await service.getRecord(req.query);
+//   res.send(result);
+// });
+
+// exports.getRecordById = asyncWrapper(async (req, res) => {
+//   const result = await service.getRecordById(req.params.id);
+//   res.send(result);
+// });
+
+// exports.patchRecordById = asyncWrapper(async (req, res) => {
+//   const result = await service.patchRecordById(req.params.id, req.body);
+//   res.send(result);
+// });
+
+// exports.deleteRecordById = asyncWrapper(async (req, res) => {
+//   const result = await service.deleteRecordById(req.params.id);
+//   res.send(result);
+// });
---
+++ back-end/InternalEventService/src/routes/api.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const controller = require("../controllers");
+
+const router = express.Router();
+
+// router.post("/record", controller.createRecord);
+// router.get("/record", controller.getRecord);
+// router.get("/record/:id", controller.getRecordById);
+// router.patch("/record/:id", controller.patchRecordById);
+// router.delete("/record/:id", controller.deleteRecordById);
+
+module.exports = router;
