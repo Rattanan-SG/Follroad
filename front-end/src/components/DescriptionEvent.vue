ee137995eb5629f68221496b01797530191bd95c
Chollachart <winnerzaza@gmail.com>
2019-08-30 15:27:36 +0700

    [UPDATE]Fix error

--- front-end/src/components/DescriptionEvent.vue
+++ front-end/src/components/DescriptionEvent.vue
@@ -2,38 +2,42 @@
   <v-container flat>
     <v-card class="mx-auto" max-width="100%" flat>
       <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+      <v-card-title>
+        <div>
+          <v-flex md2 lg2>
+            <v-img :src="infoWindow.marker.icon" max-width="40" max-height="40"></v-img>
+          </v-flex>
+          <v-flex xs12 md12 lg12>
+            <h2>{{infoWindow.marker.title}}</h2>
+            <div>โดย {{infoWindow.marker.eventCaption.contributor}}</div>
+          </v-flex>
+        </div>
+      </v-card-title>
+      <v-card-text>
+        <div>{{infoWindow.marker.description}}</div>
+      </v-card-text>
+      <v-card-text>
+        <div>เกิดเหตุการณ์ขึ้น ณ : {{infoWindow.marker.eventCaption.startTime}}</div>
+        <div>จะสิ้นสุดใน : {{infoWindow.marker.eventCaption.stopTime}}</div>
+      </v-card-text>
+      <!-- <v-flex xs12 md12 lg12> -->
 
-      <v-card flat>
-        <h2>{{infoWindow.marker.title}}</h2>
-        <v-list-item-subtitle>by {{infoWindow.marker.eventCaption.contributor}}</v-list-item-subtitle>
-        <br />
-
-        <v-flex xs12 md12 lg12>
-          <v-card-title primary-title>
-            <div>
-              <div>
-                <v-list-item-title>{{infoWindow.marker.description}}</v-list-item-title>
-              </div>
-              <br />
-              <v-divider></v-divider>
-              <br />
-              <template v-if="infoWindow.marker.eventCaption">
-                <p>เกิดเหตุการณ์ขึ้น ณ : {{infoWindow.marker.eventCaption.startTime}}</p>
-                <p>จะสิ้นสุดใน : {{infoWindow.marker.eventCaption.stopTime}}</p>
-              </template>
-              <template v-if="infoWindow.marker.searchPlaceCaption">
-                <img
-                  :src="infoWindow.marker.searchPlaceCaption.photo"
-                  :alt="infoWindow.marker.title"
-                  width="350"
-                  height="200"
-                />
-              </template>
-            </div>
-          </v-card-title>
-        </v-flex>
-      </v-card>
       <v-text-field label="แสดงความคิดเห็น" outlined></v-text-field>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn icon @click="show = !show">
+          <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+        </v-btn>
+      </v-card-actions>
+
+      <v-expand-transition>
+        <div v-show="show">
+          <v-card>
+            <v-icon>person</v-icon>รถยังไม่หายติดเลย
+          </v-card>
+        </div>
+      </v-expand-transition>
     </v-card>
   </v-container>
 </template>
