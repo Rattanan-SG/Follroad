97a79d5aa5d15bdac5957cd9c4b3b491476b04f1
Chollachart <winnerzaza@gmail.com>
2019-09-02 11:37:53 +0700

    [UPDATE]Fix error code

--- front-end/src/components/GoogleMapInfoWindow.vue
+++ front-end/src/components/GoogleMapInfoWindow.vue
@@ -26,102 +26,38 @@
 
     <v-card max-width="100%" class="mx-auto" flat>
       <v-layout>
-        <!-- <v-flex xs12 lg12 mt-5>
-          <v-list-tile avatar ripple to>
-            <v-list-tile-action>
-              <v-img :src="infoWindow.marker.icon" max-width="40" max-height="40"></v-img>
-            </v-list-tile-action>
-
-            <v-list-item-content>
-              <v-list-item-title class="headline">{{infoWindow.marker.title}}</v-list-item-title>
-              <br />
-              <v-list-item-subtitle>by {{infoWindow.marker.eventCaption.contributor}}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-tile>
-        </v-flex>-->
-        <br />
-        <br />
-        <br />
-        <v-list-tile-action>
-          <v-img :src="infoWindow.marker.icon" max-width="40" max-height="40"></v-img>
-        </v-list-tile-action>
-        <v-list-item three-line>
-          <v-list-item-content class="align-self-start">
-            <!-- <v-list-item-title class="headline mb-">{{infoWindow.marker.title}}</v-list-item-title> -->
-
-            <h3>{{infoWindow.marker.title}}</h3>
-            <v-list-item-subtitle>by {{infoWindow.marker.eventCaption.contributor}}</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-layout>
-
-      <v-layout>
         <v-flex xs12 md12 lg12>
-          <v-card-title primary-title>
-            <div>
-              <div>
-                <v-list-item-title>{{infoWindow.marker.description}}</v-list-item-title>
-              </div>
-              <br />
-              <v-divider></v-divider>
-              <br />
-              <template v-if="infoWindow.marker.eventCaption">
-                <p>เกิดเหตุการณ์ขึ้น ณ : {{infoWindow.marker.eventCaption.startTime}}</p>
-                <p>จะสิ้นสุดใน : {{infoWindow.marker.eventCaption.stopTime}}</p>
-              </template>
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-avatar>
+                <img :src="infoWindow.marker.icon" max-width="40" max-height="40" />
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>{{infoWindow.marker.title}}</v-list-tile-title>
+                <v-list-tile-sub-title>โดย {{infoWindow.marker.eventCaption.contributor}}</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+          <v-card-text>
+            <div>{{infoWindow.marker.description}}</div>
+          </v-card-text>
+          <v-card-text>
+            <div>เกิดเหตุการณ์ขึ้น ณ : {{infoWindow.marker.eventCaption.startTime}}</div>
+            <div>จะสิ้นสุดใน : {{infoWindow.marker.eventCaption.stopTime}}</div>
+          </v-card-text>
 
-              <template v-if="infoWindow.marker.searchPlaceCaption">
-                <img
-                  :src="infoWindow.marker.searchPlaceCaption.photo"
-                  :alt="infoWindow.marker.title"
-                  width="350"
-                  height="200"
-                />
-              </template>
-            </div>
-          </v-card-title>
-        </v-flex>
-      </v-layout>
+          <v-divider></v-divider>
 
-      <!-- <v-layout align-end justify-end fill-height>
-        <v-card-actions>
-          <v-btn text color="white" to="/details">See More</v-btn>
-
-          <v-spacer></v-spacer>
-        </v-card-actions>
-      </v-layout>-->
-
-      <!-- <v-layout>
-        <v-flex xs9 md9 lg9>
-          <div>
-            <v-flex md2 lg2>
-              <v-img :src="infoWindow.marker.icon" max-width="40" max-height="40"></v-img>
-            </v-flex>
-            <v-flex xs12 md12 lg12>
-              <h2>{{infoWindow.marker.title}}</h2>
-              <div>โดย {{infoWindow.marker.eventCaption.contributor}}</div>
-            </v-flex>
-          </div>
-        </v-card-title>
-        <v-card-text>
-          <div>{{infoWindow.marker.description}}</div>
-        </v-card-text>
-        <v-card-text>
-          <div>เกิดเหตุการณ์ขึ้น ณ : {{infoWindow.marker.eventCaption.startTime}}</div>
-          <div>จะสิ้นสุดใน : {{infoWindow.marker.eventCaption.stopTime}}</div>
-        </v-card-text>
-      <v-flex xs12 md12 lg12>-->
-      <v-layout>
-        <v-divider></v-divider>
-        <v-flex xs12 md12 lg12>
-          <v-btn flat icon color="blue lighten-2">
-            <v-icon>thumb_up</v-icon>
-          </v-btn>(23)
-          <v-btn flat icon color="red lighten-2">
-            <v-icon>thumb_down</v-icon>
-          </v-btn>(23)
-          <!-- <v-spacer></v-spacer> -->
-          <v-btn outline color="blue" to="/details">ดูเพิ่มเติม</v-btn>
+          <v-flex xs12 md12 lg12>
+            <v-btn flat icon color="blue lighten-2">
+              <v-icon>thumb_up</v-icon>
+            </v-btn>(23)
+            <v-btn flat icon color="red lighten-2">
+              <v-icon>thumb_down</v-icon>
+            </v-btn>(23)
+            <!-- <v-spacer></v-spacer> -->
+            <v-btn outline color="blue" to="/details">ดูเพิ่มเติม</v-btn>
+          </v-flex>
         </v-flex>
       </v-layout>
     </v-card>
