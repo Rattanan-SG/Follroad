8df81541c9f9592953a1ff37793e0282e7fe1000
Chollachart <winnerzaza@gmail.com>
2019-09-05 18:20:26 +0700

    [UPDATE]See more can open tab

--- front-end/src/components/GoogleMap/InfoWindowEvent.vue
+++ front-end/src/components/GoogleMap/InfoWindowEvent.vue
@@ -18,50 +18,45 @@
     <br />
     <br />
 
-    <v-card max-width="100%" class="mx-auto" flat>
-      <v-layout>
-        <v-flex xs12 md12 lg12>
-          <v-list>
-            <v-list-tile>
-              <v-list-tile-avatar>
-                <img :src="marker.icon" max-width="40" max-height="40" />
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>{{marker.title}}</v-list-tile-title>
-                <v-list-tile-sub-title>โดย {{marker.eventCaption.contributor}}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-          <v-card-text>
-            <div>{{marker.description}}</div>
+    <v-card max-width="100%" class="mx-auto pa-0" flat>
+      <v-list class="px-0">
+        <v-list-tile class="pa-0">
+          <v-list-tile-avatar class="pa-0">
+            <img :src="marker.icon" max-width="40" max-height="40" />
+          </v-list-tile-avatar>
+          <v-list-tile-content class="pa-0">
+            <v-list-tile-title>{{marker.title}}</v-list-tile-title>
+            <v-list-tile-sub-title>โดย {{marker.eventCaption.contributor}}</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-card-text class="pa-0">
+        <div>{{marker.description}}</div>
 
-            <br />
-            <v-divider></v-divider>
-          </v-card-text>
-          <v-card-text>
-            <div>เกิดเหตุการณ์ขึ้น ณ : {{marker.eventCaption.startTime}}</div>
-            <div>จะสิ้นสุดใน : {{marker.eventCaption.stopTime}}</div>
-          </v-card-text>
+        <br />
+        <v-divider></v-divider>
+      </v-card-text>
+      <v-card-text class="pa-0">
+        <div>เกิดเหตุการณ์ขึ้น ณ : {{marker.eventCaption.startTime}}</div>
+        <div>จะสิ้นสุดใน : {{marker.eventCaption.stopTime}}</div>
+      </v-card-text>
 
-          <v-flex xs12 md12 lg12>
-            <v-btn flat icon color="blue lighten-2" v-on:click="countLike++">
-              <v-icon>thumb_up</v-icon>
-            </v-btn>
-            {{countLike}}
-            <v-btn flat icon color="red lighten-2" v-on:click="countDislike++">
-              <v-icon>thumb_down</v-icon>
-            </v-btn>
-            {{countDislike}}
-            <!-- <v-spacer></v-spacer> -->
-            <v-btn outline color="blue" to="/details">ดูเพิ่มเติม</v-btn>
-          </v-flex>
-        </v-flex>
-      </v-layout>
+      <v-btn flat icon color="blue lighten-2" v-on:click="countLike++">
+        <v-icon>thumb_up</v-icon>
+      </v-btn>
+      {{countLike}}
+      <v-btn flat icon color="red lighten-2" v-on:click="countDislike++">
+        <v-icon>thumb_down</v-icon>
+      </v-btn>
+      {{countDislike}}
+      <!-- <v-spacer></v-spacer> -->
+      <v-btn outline color="blue" to="/details" @click="setRouterView(true)">ดูเพิ่มเติม</v-btn>
     </v-card>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   name: "InfoWindowEvent",
   props: {
@@ -73,6 +68,9 @@ export default {
       countLike: 0,
       countDislike: 0
     };
+  },
+  methods: {
+    ...mapActions("route", ["setRouterView"])
   }
 };
 </script>
