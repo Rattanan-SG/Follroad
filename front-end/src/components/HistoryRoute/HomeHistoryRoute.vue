60c8a190a6ff06a6bc0b27fa14a1e158792bd3a7
Chollachart <winnerzaza@gmail.com>
2019-09-09 17:57:50 +0700

    [UPDATE]History route to beautiful

--- front-end/src/components/HistoryRoute/HomeHistoryRoute.vue
+++ front-end/src/components/HistoryRoute/HomeHistoryRoute.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container>
-    <v-list>
-      <v-layout grid-list-md text-xs-center>
+  <div>
+    <!-- <v-list> -->
+    <!-- <v-layout grid-list-md text-xs-center>
         <v-flex xs1>
           <div>
             <v-btn flat to="/profile">
@@ -14,8 +14,8 @@
             <v-icon>person</v-icon>
           </v-btn>
         </v-flex>
-      </v-layout>
-      <v-layout grid-list-md text-xs-center>
+    </v-layout>-->
+    <!-- <v-layout grid-list-md text-xs-center>
         <v-flex xs12>
           <h1 class="Name">Vertical Duke</h1>
         </v-flex>
@@ -24,48 +24,62 @@
         <v-flex xs12 grid-list-md text-xs-center>
           <h4 class="e-mail">vertical.duke@gmail.com</h4>
         </v-flex>
-      </v-layout>
-      <v-layout>
-        <v-flex xs12 lg12 mt-5>
-          <v-list-tile avatar ripple>
-            <v-list-tile-action>
-              <v-icon size="35px" color="indigo">location_on</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <h3>Home --- KMUTT</h3>
-              <v-list-tile-sub-title>CCC</v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-flex>
-      </v-layout>
-
-      <v-layout>
-        <v-flex xs12 lg12 mt-0>
-          <v-list-tile avatar ripple>
-            <v-list-tile-action>
-              <v-icon size="35px" color="indigo">location_on</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <h3>Home --- KMUTT</h3>
-              <v-list-tile-sub-title>CCC</v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-flex>
-      </v-layout>
-
-      <v-layout>
-        <v-flex xs12 lg12 mt-0>
-          <v-list-tile avatar ripple>
-            <v-list-tile-action>
-              <v-icon size="35px" color="indigo">location_on</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <h3>Home --- KMUTT</h3>
-              <v-list-tile-sub-title>CCC</v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-flex>
-      </v-layout>
-    </v-list>
-  </v-container>
+    </v-layout>-->
+    <v-layout>
+      <v-flex xs6 lg6 md6>
+        <v-btn flat to="/profile">
+          <v-icon blue>arrow_back_ios</v-icon>โปรไฟล์
+        </v-btn>
+      </v-flex>
+      <v-flex xs10 lg10 md10 mt-2>
+        <h3>เส้นทางที่บันทึก</h3>
+      </v-flex>
+      <!-- <v-flex xs1 md1 lg1></v-flex> -->
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 lg12 mt-5>
+        <v-list-tile avatar ripple>
+          <v-list-tile-action>
+            <v-icon size="35px" color="indigo">location_on</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <h3>Home --- KMUTT</h3>
+            <v-list-tile-sub-title>CCC</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-flex>
+    </v-layout>
+    <v-divider></v-divider>
+    <v-layout>
+      <v-flex xs12 lg12 mt-0>
+        <v-list-tile avatar ripple>
+          <v-list-tile-action>
+            <v-icon size="35px" color="indigo">location_on</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <h3>Home --- KMUTT</h3>
+            <v-list-tile-sub-title>CCC</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-flex>
+    </v-layout>
+    <v-divider></v-divider>
+    <v-layout>
+      <v-flex xs12 lg12 mt-0>
+        <v-list-tile avatar ripple>
+          <v-list-tile-action>
+            <v-icon size="35px" color="indigo">location_on</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <h3>Home --- KMUTT</h3>
+            <v-list-tile-sub-title>CCC</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-flex>
+    </v-layout>
+    <v-divider></v-divider>
+    <!-- </v-list> -->
+  </div>
 </template>
+<style>
+</style>
