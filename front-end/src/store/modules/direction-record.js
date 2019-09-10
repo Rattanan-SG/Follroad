b506bd15bb18f9cfe671f05778edce9a0e77600c
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-09-10 19:03:36 +0700

    [WORKING] start history route

---
+++ front-end/src/store/modules/direction-record.js
@@ -0,0 +1,40 @@
+import directionRecord from "@/api/direction-record";
+const state = {
+  directionRecords: []
+};
+
+const getters = {
+  directionRecords: state => {
+    return state.directionRecords;
+  },
+  directionRecordById: state => id => {
+    return state.directionRecords.find(record => record.id == id);
+  }
+};
+
+const actions = {
+  fetchDirectionRecordsByUid: ({ commit }, uid) => {
+    directionRecord
+      .getRecords({ uid })
+      .then(data => {
+        commit("SET_DIRECTION_RECORDS", data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+};
+
+const mutations = {
+  SET_DIRECTION_RECORDS: (state, directionRecords) => {
+    state.directionRecords = directionRecords;
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+};
