53e76d211898331868dca2c5e3af2941b4843299
Guntapat Nakviroj <guntapaan116@gmail.com>
2019-09-17 16:14:57 +0700

    [Update] His

--- front-end/src/components/HistoryRoute/HistoryRouteList.vue
+++ front-end/src/components/HistoryRoute/HistoryRouteList.vue
@@ -3,21 +3,55 @@
     <div v-if="loading" class="text-xs-center">
       <v-progress-circular :size="100" :width="7" color="primary" indeterminate></v-progress-circular>
     </div>
+
     <v-list v-if="!!directionRecords.length" three-line>
       <template v-for="(record, index) in directionRecords">
         <v-list-tile class="mt-1" :key="index" avatar ripple @click="startHistoryRoute(record)">
           <v-list-tile-content>
-            <v-list-tile-title>{{ record.name }}</v-list-tile-title>
-            <v-list-tile-sub-title
-              class="text--primary"
-            >จาก {{ record.start.name }} --> {{ record.destination.name }}</v-list-tile-sub-title>
-            <v-list-tile-sub-title>แก้ไข้ล่าสุด ณ {{ formatUpdatedAt(record.updatedAt) }}</v-list-tile-sub-title>
+            <v-card-actions>
+              <v-flex mt-3 ml-3 xs1 md1 lg1>
+                <v-icon size="40px">home</v-icon>
+              </v-flex>
+            </v-card-actions>
+            <v-flex ml-1 xs9 md9 lg9>
+              <v-list-tile-title>{{ record.name }}</v-list-tile-title>
+              <v-list-tile-sub-title
+                class="text--primary"
+              >จาก {{ record.start.name }} --> {{ record.destination.name }}</v-list-tile-sub-title>
+              <v-list-tile-sub-title>แก้ไข้ล่าสุด ณ {{ formatUpdatedAt(record.updatedAt) }}</v-list-tile-sub-title>
+            </v-flex>
           </v-list-tile-content>
         </v-list-tile>
         <v-divider :key="`d${index}`"></v-divider>
       </template>
     </v-list>
-    <div v-if="!loading && !directionRecords.length">
+
+    <!-- 
+<v-list v-if="!!directionRecords.length" three-line>
+        <template v-for="(record, index) in directionRecords">
+          <v-divider :key="`d${index}`"></v-divider>
+          <v-layout flat>
+            <v-flex mt-3 ml-3 xs1 md1 lg1>
+              <v-icon size="40px">home</v-icon>
+            </v-flex>
+            <v-flex ml-1 xs9 md9 lg9>
+              <v-list-tile class="mt-1" :key="index" avatar ripple @click="startHistoryRoute(record)">
+                <v-list-tile-content>
+                  <h4>{{ record.name }}</h4>
+                 <v-list-tile-sub-title
+              class="text--primary"
+            >จาก {{ record.start.name }}  {{ record.destination.name }}</v-list-tile-sub-title>
+            <v-list-tile-sub-title>แก้ไข้ล่าสุด ณ {{ formatUpdatedAt(record.updatedAt) }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-flex>
+          </v-layout>
+          <v-divider :key="`d${index}`"></v-divider>
+
+        </template>
+    </v-list>-->
+
+    <!-- <div v-if="!loading && !directionRecords.length">
       <v-flex xs12 lg12 md12 pa-2>
         <v-card class="mx-auto" color="#26c6da" dark flat>
           <v-card-title>
@@ -30,47 +64,7 @@
           </v-card-title>
         </v-card>
       </v-flex>
-      <!-- <v-list two-line>
-        <template>
-          <v-divider></v-divider>
-          <v-layout>
-            <v-flex mt-3 ml-3 xs1 md1 lg1>
-              <v-icon size="40px">home</v-icon>
-            </v-flex>
-            <v-flex ml-1 xs9 md9 lg9>
-              <v-list-tile>
-                <v-list-tile-content>
-                  <h4>บ้านไปมอ</h4>
-                  <v-list-tile-sub-title
-                    class="text--primary"
-                  >จากตำแหน่งปัจจุบัน ไป เซ็นทรัลพลาซา พระราม 2</v-list-tile-sub-title>
-                  <v-list-tile-sub-title>แก้ไขล่าสุด ณ 13/09/2019, 10:13:16</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
-            </v-flex>
-          </v-layout>
-          <v-divider></v-divider>
-
-          <v-layout>
-            <v-flex mt-3 ml-3 xs1 md1 lg1>
-              <v-icon size="40px">home</v-icon>
-            </v-flex>
-            <v-flex ml-1 xs9 md9 lg9>
-              <v-list-tile>
-                <v-list-tile-content>
-                  <h4>บ้านไปมอ</h4>
-                  <v-list-tile-sub-title
-                    class="text--primary"
-                  >จากตำแหน่งปัจจุบัน ไป เซ็นทรัลพลาซา พระราม 2</v-list-tile-sub-title>
-                  <v-list-tile-sub-title>แก้ไขล่าสุด ณ 13/09/2019, 10:13:16</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
-            </v-flex>
-          </v-layout>
-          <v-divider></v-divider>
-        </template>
-      </v-list>-->
-    </div>
+    </div>-->
   </div>
 </template>
 <script>
