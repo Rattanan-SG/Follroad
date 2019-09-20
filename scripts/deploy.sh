3f93ae0897886c51d3bc486be28267d6ef8819ea
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-09-20 15:58:17 +0700

    [TEST] new deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,8 @@
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 
-add-apt-repository ppa:eugenesan/ppa
-apt-get update
-apt-get install jq -y
-
-curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | sudo tee -a /usr/bin/ecs-deploy
-sudo chmod +x /usr/bin/ecs-deploy
-
-ecs-deploy -c $CLUSTER_NAME -n event -i $DOCKER_HUB_USER/follroad-event:latest --skip-deployments-check --force-new-deployment
-ecs-deploy -c $CLUSTER_NAME -n message-queue -i $DOCKER_HUB_USER/follroad-message-queue:latest --skip-deployments-check --force-new-deployment
-ecs-deploy -c $CLUSTER_NAME -n notification -i $DOCKER_HUB_USER/follroad-notification:latest --skip-deployments-check --force-new-deployment
-ecs-deploy -c $CLUSTER_NAME -n direction-record -i $DOCKER_HUB_USER/follroad-direction-record:latest --skip-deployments-check --force-new-deployment
-ecs-deploy -c $CLUSTER_NAME -n geometry -i $DOCKER_HUB_USER/follroad-geometry:latest --skip-deployments-check --force-new-deployment
+aws ecs update-service --cluster $CLUSTER_NAME --service event --force-new-deployment
+aws ecs update-service --cluster $CLUSTER_NAME --service message-queue --force-new-deployment
+aws ecs update-service --cluster $CLUSTER_NAME --service notification --force-new-deployment
+aws ecs update-service --cluster $CLUSTER_NAME --service direction-record --force-new-deployment
+aws ecs update-service --cluster $CLUSTER_NAME --service geometry --force-new-deployment
