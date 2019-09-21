8fd7d47fa060df763ed9de6d738b79c6cf81d4d0
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-09-21 13:37:21 +0700

    [FIX] remove new promise

--- front-end/src/store/modules/direction-record.js
+++ front-end/src/store/modules/direction-record.js
@@ -18,15 +18,19 @@ const getters = {
 
 const actions = {
   fetchDirectionRecordsByUid: ({ commit }, uid) => {
-    return new Promise(resolve => {
-      directionRecord.getRecords({ uid }).then(data => {
-        commit("SET_DIRECTION_RECORDS", data);
-        resolve();
-      });
-    });
+    return directionRecord
+      .getRecords({ uid })
+      .then(data => commit("SET_DIRECTION_RECORDS", data));
   },
-  updateDirectionRecordById: ({ commit }, payloadWithId) => {
-    commit("UPDATE_DIRECTION_RECORD_BY_ID", payloadWithId);
+  updateDirectionRecordById: ({ commit }, { _id, data }) => {
+    return directionRecord
+      .patchRecordById(_id, data)
+      .then(data => commit("UPDATE_DIRECTION_RECORD_BY_ID", data));
+  },
+  deleteDirectionRecordById: ({ commit }, _id) => {
+    return directionRecord
+      .deleteRecordById(_id)
+      .then(({ _id }) => commit("DELETE_DIRECTION_RECORD_BY_ID", _id));
   },
   setHistoryMode: ({ commit }, historyMode) => {
     commit("SET_HISTORY_MODE", historyMode);
@@ -37,10 +41,18 @@ const mutations = {
   SET_DIRECTION_RECORDS: (state, directionRecords) => {
     state.directionRecords = directionRecords;
   },
-  UPDATE_DIRECTION_RECORD_BY_ID: (state, { id, data }) => {
-    const index = state.directionRecords.findIndex(record => record._id === id);
+  UPDATE_DIRECTION_RECORD_BY_ID: (state, { _id, data }) => {
+    const index = state.directionRecords.findIndex(
+      record => record._id === _id
+    );
     const newRecord = { ...state.directionRecords[index], ...data };
     state.directionRecords[index] = newRecord;
+  },
+  DELETE_DIRECTION_RECORD_BY_ID: (state, _id) => {
+    const index = state.directionRecords.findIndex(
+      record => record._id === _id
+    );
+    state.directionRecords.splice(index, 1);
   },
   SET_HISTORY_MODE: (state, historyMode) => {
     state.historyMode = historyMode;
