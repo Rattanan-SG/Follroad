c9100ad94339ef1d7baaea9e001fccf2bb02ecc4
Chollachart <winnerzaza@gmail.com>
2019-09-22 00:10:08 +0700

    [EDIT]edit login dialog to beautiful !!

--- front-end/src/components/Feedback/LoginDialog.vue
+++ front-end/src/components/Feedback/LoginDialog.vue
@@ -1,18 +1,26 @@
 <template>
-  <v-layout row justify-center>
-    <v-dialog v-model="loginDialog" max-width="400">
-      <v-card dark>
-        <v-card-text class="title">
-          <v-icon color="blue">info</v-icon>
-          <span>กรุณาเข้าสู่ระบบเพื่อดำเนินการต่อ</span>
-        </v-card-text>
-        <v-divider></v-divider>
+  <v-layout row>
+    <v-dialog v-model="loginDialog" scrollable max-width="70%" max-height="100%">
+      <!-- <v-flex xs12 lg12 md12> -->
+      <v-card>
+        <v-toolbar color="blue" dark flat>
+          <v-icon>lock</v-icon>
+          <v-toolbar-title>กรุณาเข้าสู่ระบบ</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-card-text>หากต้องการจะใช้งานความสามารถนี้ในส่วนนี้ต้องเข้าสู่ระบบก่อน</v-card-text>
+        <!-- <v-divider></v-divider> -->
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="red" flat @click="loginDialog = false">ยกเลิก</v-btn>
-          <v-btn color="green darken-1" flat @click="login" :loading="loginLoading">เข้าสู่ระบบ</v-btn>
+          <v-btn outline color="red" flat @click="loginDialog = false">
+            <v-icon>close</v-icon>ยกเลิก
+          </v-btn>
+          <v-btn outline color="green darken-1" flat @click="login" :loading="loginLoading">
+            <v-icon>check</v-icon>เข้าสู่ระบบ
+          </v-btn>
         </v-card-actions>
       </v-card>
+      <!-- </v-flex> -->
     </v-dialog>
   </v-layout>
 </template>
