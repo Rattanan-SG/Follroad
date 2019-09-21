d37f95a378b5c31b1d7e1dc46488f712e6009e75
Choachart <winnerzaza@gmail.com>
2019-09-21 17:19:20 +0700

    [EDIT]back to expansion panel but fix height

--- front-end/src/components/Search/SearchFeedPanel.vue
+++ front-end/src/components/Search/SearchFeedPanel.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <!-- <v-expansion-panel v-if="specificEvents"> -->
-    <!-- <v-expansion-panel-content> -->
-    <!-- <template v-slot:header>
-        <div>จำนวนเหตุการณ์ที่เกี่ยวข้องกับเส้นทางนี้ ({{specificEvents.length}})</div>
-    </template>-->
-    <v-card v-if="specificEvents">
-      <v-card-text>จำนวนเหตุการณ์ที่เกี่ยวข้องกับเส้นทางนี้ ({{specificEvents.length}})</v-card-text>
-      <v-list two-line>
-        <template v-for="(event, index) in specificEvents">
-          <HomeFeedItem :key="index" :event="event"></HomeFeedItem>
-        </template>
-      </v-list>
-    </v-card>
-    <!-- </v-expansion-panel-content> -->
-    <!-- </v-expansion-panel> -->
+    <v-expansion-panel v-if="specificEvents">
+      <v-expansion-panel-content>
+      <template v-slot:header>
+          <div>จำนวนเหตุการณ์ที่เกี่ยวข้องกับเส้นทางนี้ ({{specificEvents.length}})</div>
+      </template>
+      <!-- <v-card v-if="specificEvents"> -->
+        <!-- <v-card-text>จำนวนเหตุการณ์ที่เกี่ยวข้องกับเส้นทางนี้ ({{specificEvents.length}})</v-card-text> -->
+        <v-list two-line>
+          <template v-for="(event, index) in specificEvents">
+            <HomeFeedItem :key="index" :event="event"></HomeFeedItem>
+          </template>
+        </v-list>
+      <!-- </v-card> -->
+      </v-expansion-panel-content> 
+    </v-expansion-panel>
   </div>
 </template>
 
@@ -35,6 +35,6 @@ export default {
 <style scoped>
 .v-list {
   overflow: auto;
-  max-height: 15vh;
+  max-height: 20vh;
 }
 </style>
