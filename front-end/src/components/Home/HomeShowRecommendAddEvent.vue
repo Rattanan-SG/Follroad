097879263c3434aa04e9af2bcba94c629b94ad7d
Chollachart <winnerzaza@gmail.com>
2019-09-24 11:26:54 +0700

    [EDIT]Fix position recommend add event

--- front-end/src/components/Home/HomeShowRecommendAddEvent.vue
+++ front-end/src/components/Home/HomeShowRecommendAddEvent.vue
@@ -1,10 +1,14 @@
 <template>
-<v-flex xs12 lg12 md12>
+  <v-flex xs12 lg12 md12>
     <v-card class="mx-auto" color="#26c6da" dark flat>
-        <v-card-title>
-            <v-icon left>place</v-icon>
-            <h3 class="font-weight-light">เลือกตำแหน่งบนแผนที่เพื่อแจ้งเหตุการณ์</h3>
-        </v-card-title>
+      <v-card-title>
+        <v-flex xs1 lg1 md1>
+          <v-icon left>place</v-icon>
+        </v-flex>
+        <v-flex xs10 lg10 md10>
+          <h3 class="font-weight-light">เลือกตำแหน่งบนแผนที่เพื่อแจ้งเหตุการณ์</h3>
+        </v-flex>
+      </v-card-title>
     </v-card>
-</v-flex>
+  </v-flex>
 </template>
