897e55dc3de97c1a4f7c1fd4de27c2d780a3d794
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-09-25 17:41:53 +0700

    [REMOVE] picture api

--- back-end/EventService/src/controllers/index.js
+++ back-end/EventService/src/controllers/index.js
@@ -1,11 +1,9 @@
 const event = require("./event");
 const comment = require("./comment");
 const feedback = require("./feedback");
-const picture = require("./picture");
 
 module.exports = {
   event,
   comment,
-  feedback,
-  picture
+  feedback
 };
--- back-end/EventService/src/routes/api/index.js
+++ back-end/EventService/src/routes/api/index.js
@@ -2,11 +2,10 @@ const express = require("express");
 const event = require("./event");
 const comment = require("./comment");
 const feedback = require("./feedback");
-const picture = require("./picture");
 
 const router = express.Router();
 
-const apis = [event, comment, feedback, picture];
+const apis = [event, comment, feedback];
 
 router.use(apis);
 
