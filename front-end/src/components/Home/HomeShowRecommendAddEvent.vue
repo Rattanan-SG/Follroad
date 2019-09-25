2bcc08e7235767e475ed934a75cc99245fc3b3c4
Chollachart <winnerzaza@gmail.com>
2019-09-25 19:24:19 +0700

    [EDIT]Word and responsive

--- front-end/src/components/Home/HomeShowRecommendAddEvent.vue
+++ front-end/src/components/Home/HomeShowRecommendAddEvent.vue
@@ -2,11 +2,11 @@
   <v-flex xs12 lg12 md12>
     <v-card class="mx-auto" color="#26c6da" dark flat>
       <v-card-title>
-        <v-flex xs1 lg1 md1>
+        <v-flex xs2 lg1 md1 sm1>
           <v-icon left>place</v-icon>
         </v-flex>
-        <v-flex xs10 lg10 md10>
-          <h3 class="font-weight-light">เลือกตำแหน่งบนแผนที่เพื่อแจ้งเหตุการณ์</h3>
+        <v-flex xs10 lg11 md11 sm12>
+          <h3 class="font-weight-light">กดบนแผนที่เพื่อแจ้งเหตุการณ์</h3>
         </v-flex>
       </v-card-title>
     </v-card>
