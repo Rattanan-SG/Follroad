923c89ea22b733330c53bb1f5e7ba2659a5925bc
Chollachart <winnerzaza@gmail.com>
2019-09-27 15:06:14 +0700

    [EDIT]Recommend add event

--- front-end/src/components/Home/HomeShowRecommendAddEvent.vue
+++ front-end/src/components/Home/HomeShowRecommendAddEvent.vue
@@ -1,11 +1,12 @@
 <template>
   <v-flex xs12 lg12 md12>
-    <v-card class="mx-auto" dark flat>
+    <v-card class="mx-auto" color="blue lighten-5" flat>
       <v-card-title>
-        <v-flex xs2 lg1 md1 sm1>
-          <v-icon left color="orange">place</v-icon>
+        <v-flex xs1 lg1 md1 sm1>
+          <v-icon left color="red">place</v-icon>
         </v-flex>
-        <v-flex xs10 lg11 md11 sm12>
+        <!-- <v-flex xs1 md1 sm1 lg1></v-flex> -->
+        <v-flex xs10 lg10 md10 sm10>
           <h3 class="font-weight-light">กดบนแผนที่เพื่อแจ้งเหตุการณ์</h3>
         </v-flex>
       </v-card-title>
