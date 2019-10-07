96594e0591efcea57c2870ca69c61e2bd6b077c9
Choachart <winnerzaza@gmail.com>
2019-10-07 21:36:04 +0700

    [UPDATE]Change code in close view toolbar to navbar

--- front-end/src/components/Navbar/CloseViewToolBar.vue
+++ front-end/src/components/Navbar/CloseViewToolBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card color="blue darken-3" flat>
+  <!-- <v-card color="blue darken-3" flat>
     <v-container text-xs-center class="pa-0">
       <v-layout row wrap justify-center align-center>
         <v-flex xs2>
@@ -17,7 +17,27 @@
         </v-flex>
       </v-layout>
     </v-container>
-  </v-card>
+  </v-card> -->
+  <div>
+    <v-toolbar color="blue darken-3" flat>
+      <v-layout align-center>
+        <v-flex xs2 md2 lg2 sm2>
+          <v-btn icon v-if="!!leftIcon" @click="leftAction">
+            <v-icon color="white">{{leftIcon}}</v-icon>
+          </v-btn>
+        </v-flex>
+        <v-flex xs3 md3 lg3 sm3></v-flex>
+        <v-flex xs7 md7 lg7 sm7>
+          <v-toolbar-title class="white--text">{{title}}</v-toolbar-title>
+        </v-flex>
+        <v-flex xs1 md1 lg1 sm1>
+          <v-btn icon @click="setRouterView(false)">
+            <v-icon color="white">close</v-icon>
+          </v-btn>
+        </v-flex>
+      </v-layout>
+    </v-toolbar>
+  </div>
 </template>
 
 <script>
