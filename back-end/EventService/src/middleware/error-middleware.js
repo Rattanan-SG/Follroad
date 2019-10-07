d13fe666db26e0690e52b0fdc064472b2b3b014f
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-10-07 16:18:11 +0700

    [FIX] error-middleware in 5 service

--- back-end/EventService/src/middleware/error-middleware.js
+++ back-end/EventService/src/middleware/error-middleware.js
@@ -2,25 +2,39 @@ const CustomError = require("../utils/custom-error");
 const { logError } = require("../utils/logger");
 
 module.exports = async (error, req, res, next) => {
-  if (error.name === "UnauthorizedError") {
-    logError("Unauthorized Error", {
-      req: req.originalUrl,
+  const { name, status, code, message } = error;
+  if (error instanceof CustomError) {
+    logError("Error happen", {
+      request: `${req.method} ${req.originalUrl}`,
+      error
+    });
+    return res.status(status).send({
+      error: code,
+      message
+    });
+  } else if (name === "UnauthorizedError") {
+    logError("Unauthorized error", {
+      request: `${req.method} ${req.originalUrl}`,
       error
     });
     return res.status(401).send(error);
-  } else if (error instanceof CustomError) {
-    logError("Error happen", {
-      req: req.originalUrl,
-      error
+  } else if (name === "SequelizeUniqueConstraintError") {
+    const { errors } = error;
+    if (errors) errors.forEach(v => delete v.instance);
+    logError("Database error", {
+      request: `${req.method} ${req.originalUrl}`,
+      name: error.toString(),
+      error: errors
     });
-    return res.status(error.status).send({
-      error: error.code,
-      description: error.message
+    return res.status(400).send({
+      error: "DATABASE",
+      description: error.toString(),
+      message: errors
     });
   }
   console.log(error);
-  logError("Unplanned Error happen", {
-    req: req.originalUrl,
+  logError("Unplanned error happen", {
+    request: `${req.method} ${req.originalUrl}`,
     error: error.toString()
   });
   return res.status(500).send({
--- back-end/MessageQueueServiceProducer/src/middleware/error-middleware.js
+++ back-end/MessageQueueServiceProducer/src/middleware/error-middleware.js
@@ -2,8 +2,8 @@ const { logError } = require("../utils/logger");
 
 module.exports = async (error, req, res, next) => {
   console.log(error);
-  logError("Unplanned Error happen", {
-    req: req.originalUrl,
+  logError("Unplanned error happen", {
+    request: `${req.method} ${req.originalUrl}`,
     error: error.toString()
   });
   return res.status(500).send({
