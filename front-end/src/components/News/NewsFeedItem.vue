65c5d9572b77db931f0d5c2e6770bbdf76a5ad85
Guntapat Nakviroj <guntapaan116@gmail.com>
2019-10-13 19:20:22 +0700

    [Update] Newfeed

--- front-end/src/components/News/NewsFeedItem.vue
+++ front-end/src/components/News/NewsFeedItem.vue
@@ -52,10 +52,54 @@
           {{countDislike}}
         </v-flex>
         <v-spacer></v-spacer>
-        <v-flex xs2 md2 lg2>
-          <v-btn outline color="blue" to="/details" @click="setRouterView(true)">แสดงความคิดเห็น</v-btn>
+        <v-flex xs2 md2 lg2 mr-5>
+          <v-btn outline color="blue" @click="show = !show">แสดงความคิดเห็น</v-btn>
         </v-flex>
       </v-card-actions>
+      <v-slide-y-transition>
+        <v-layout v-show="show">
+          <v-flex xs2 md2 lg2 pt-3 pl-3 mr-2>
+            <v-avatar size="30px">
+              <img v-if="isAuthenticated" :src="profile.picture" alt="avatar" />
+              <v-icon v-else color="primary" medium>person</v-icon>
+            </v-avatar>
+          </v-flex>
+          <v-flex xs7 md7 lg7>
+            <v-textarea
+              v-model="comment"
+              label="แสดงความคิดเห็น"
+              rows="1"
+              outline
+              clearable
+              auto-grow
+              :disabled="!isAuthenticated"
+            ></v-textarea>
+          </v-flex>
+          <v-flex xs2 md2 lg2 pt-2 ml-3>
+            <v-btn icon class="ma-0">
+              <v-icon color="primary">send</v-icon>
+            </v-btn>
+          </v-flex>
+          <v-flex xs12 md12 lg12>
+            <v-list-tile>
+              <v-flex mt-1 xs1 md1 lg1>
+                <v-avatar size="30px">
+                  <img src="@/assets/logo.svg" alt="avatar" />
+                </v-avatar>
+              </v-flex>
+              <v-flex ml-3 xs9 md9 lg9>
+                <v-list-tile avatar>
+                  <v-list-tile-content>
+                    <h3>ยังติดอยู่เลย</h3>
+
+                    <v-list-tile-sub-title>ไม่ขยับมา 1 ชั่วโมงแล้ว</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-flex>
+            </v-list-tile>
+          </v-flex>
+        </v-layout>
+      </v-slide-y-transition>
     </v-layout>
   </v-card>
 </template>
@@ -65,6 +109,9 @@ import { mapGetters } from "vuex";
 import eventConstant from "@/utilitys/eventConstant";
 export default {
   name: "NewsFeedItem",
+  data: () => ({
+    show: false
+  }),
   props: {
     event: Object
   },
