60cc4183321640ee1158a55981ae25ff7441abd0
Choachart <winnerzaza@gmail.com>
2019-10-15 00:59:15 +0700

    [TEST]New ui for home page รูปชัดขึ้น เปลี่ยนสี ปรับcode(ชอบมั้ยครับ)

--- front-end/src/components/Home/HomeFeedItem.vue
+++ front-end/src/components/Home/HomeFeedItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-list-tile avatar @click="toggleInfoWindow" v-ripple>
+  <!-- <v-list-tile avatar @click="toggleInfoWindow" v-ripple>
     <v-list-tile-avatar>
       <v-img :src="getIcon()" max-width="40" max-height="40"></v-img>
     </v-list-tile-avatar>
@@ -7,7 +7,18 @@
       <v-list-tile-title>{{event.title}}</v-list-tile-title>
       <v-list-tile-sub-title>เกิดขึ้น ณ {{ event.start | luxon:locale('short') }}</v-list-tile-sub-title>
     </v-list-tile-content>
-  </v-list-tile>
+  </v-list-tile> -->
+  <v-card flat class="mb-4" @click="toggleInfoWindow" v-ripple>
+    <v-layout>
+      <v-flex xs1 md1 lg1 ml-3>
+        <v-img :src="getIcon()" max-width="35" max-height="35" left></v-img>
+      </v-flex>
+      <v-flex xs11 md11 lg11 ml-3>
+        <h4 class="blue--text">{{event.title}}</h4>
+        <div>เกิดขึ้น ณ {{ event.start | luxon:locale('short') }}</div>
+      </v-flex>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
