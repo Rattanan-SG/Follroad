46686bbbf2c4c1e770f89ae6cc065a3aaf612998
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-10-16 16:27:00 +0700

    [UPDATE] direction-record model

--- back-end/DirectionRecordService/src/models/direction-record.js
+++ back-end/DirectionRecordService/src/models/direction-record.js
@@ -4,7 +4,12 @@ const directionRecordSchema = new mongoose.Schema(
   {
     uid: {
       type: String,
-      index: true
+      index: true,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
     },
     start: {
       name: { type: String, required: true },
@@ -20,33 +25,51 @@ const directionRecordSchema = new mongoose.Schema(
         lng: { type: Number, required: true }
       }
     },
-    name: {
-      type: String,
-      required: true
-    },
-    notificationRoutes: [Number],
     direction: {
       type: mongoose.Schema.Types.Mixed,
       required: true
     },
-    schedule: {
-      time: { type: Date },
-      day: {
-        type: String,
-        enum: [
-          "Sunday",
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday"
-        ]
-        // required: true
+    notificationRoutes: [Number],
+    notificationTime: [
+      {
+        time: { type: Date, required: true },
+        days: [
+          {
+            type: String,
+            enum: [
+              "Sunday",
+              "Monday",
+              "Tuesday",
+              "Wednesday",
+              "Thursday",
+              "Friday",
+              "Saturday"
+            ]
+          }
+        ],
+        type: {
+          type: String,
+          enum: ["Onetime", "Schedule"],
+          required: true
+        },
+        ongoing: {
+          type: Boolean,
+          default: true,
+          required: true
+        }
       }
-    }
+    ]
   },
   { timestamps: true }
 );
+
+// TODO: check not same time and day and if type Onetime time must more than now
+// directionRecordSchema.pre("validate", function(next) {
+//   console.log(111, this.notificationTime);
+//   if (this.startDate > this.endDate) {
+//     this.invalidate('startDate', 'Start date must be less than end date.', this.startDate);
+//   }
+//   next();
+// });
 
 module.exports = mongoose.model("DirectionRecord", directionRecordSchema);
